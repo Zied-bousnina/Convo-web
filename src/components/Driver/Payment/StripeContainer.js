88f13484344432aeafb9f57759d84e4437c2f75d
Zied bousnina <zbousnina@yahoo.com>
2024-01-29 22:07:47 +0100

    b

--- src/components/Driver/Payment/StripeContainer.js
+++ src/components/Driver/Payment/StripeContainer.js
@@ -4,7 +4,7 @@ import React from 'react'
 import PaymentForm from './PaymentForm'
 
 function StripeContainer() {
-  const apiKey = `pk_test_51OdwexAFbclQdyveAdsJIQfP5zVc2EzTvA7r682Jd4PCio1sBuxWAd6pNBG11ODCeodAiwNxdrMOoQhINWmOljfD005S4ERcg0`;
+  const apiKey = `pk_live_51OdwexAFbclQdyveddA1UnYh3py2azs5N0PGMaw0EuSwMoSwPLNyoZyqX7tqLUn5nDJl2t0hxFkYVbHJDg54NzjG00nDqskg7u`;
   const stripeTestPromise = loadStripe(
     apiKey
   )
